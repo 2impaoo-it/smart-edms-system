75d92b3efb7381878da0ddfaf7dd2930acafc5f3
2impaoo-it <baoga271104@gmail.com>
2026-03-16 14:32:34 +0700

    Task [BE-FILE-02]: API Upload & Metadata Persistence

--- services/core-service/src/main/java/com/smartedms/controller/DocumentController.java
+++ services/core-service/src/main/java/com/smartedms/controller/DocumentController.java
@@ -1,12 +1,18 @@
 package com.smartedms.controller;
 
+import com.smartedms.entity.Document;
 import com.smartedms.service.DocumentService;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/documents")
@@ -16,6 +22,14 @@ public class DocumentController {
 
     public DocumentController(DocumentService documentService) {
         this.documentService = documentService;
+    }
+
+    @PostMapping(consumes = "multipart/form-data")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Document upload(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam(value = "folderId", required = false) Long folderId) {
+        return documentService.uploadPdf(file, folderId);
     }
 
     @GetMapping("/{id}/view")
